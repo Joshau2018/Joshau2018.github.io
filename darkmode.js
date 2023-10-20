e9199501adc805f7863ca263de7bc1932f7d0d4c
Joshau2018 <113706963+Joshau2018@users.noreply.github.com>
2023-10-20 13:32:29 +0000

    new file:   contact.html 	new file:   darkmode.js 	new file:   fancy.css 	modified:   index.html     modified:   webfolio.html

---
+++ darkmode.js
@@ -0,0 +1,18 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+  function darkModeToggle() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+  }
+
+  function darkMode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+  }
