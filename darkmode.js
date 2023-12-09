1d3caaf8851da544a51445d003007bf626f5fdc6
Joshau2018 <Jfklari772@gmail.com>
2023-12-09 17:59:58 -0600

    memior: darkmode/paper/ background/ navbarissues

--- darkmode.js
+++ darkmode.js
@@ -1,18 +1,9 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
-
 function darkModeToggle() {
     var element = document.body;
     element.classList.toggle("dark-mode");
-}
-
-function darkMode() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
+    
+    var sections = document.getElementsByClassName("dark-mode-section");
+    for (var i = 0; i < sections.length; i++) {
+        sections[i].classList.toggle("dark-mode-section-active");
+    }
 }
